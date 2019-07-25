a40689750caa9cd1c9cfea845d576a7ee17a2ccb
Wonhee Jung <841438+freesoft@users.noreply.github.com>
2019-07-25 16:47:36 -0700

    Update load.js

--- load.js
+++ load.js
@@ -3,8 +3,10 @@ const yearEnd = 2017;
 const totalNoOfCountriesToLoad = 400;
 
 const margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 800 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+    svgWidth = 800,
+    svgHeight = 600,
+    width = svgWidth - margin.left - margin.right,
+    height = svgHeight - margin.top - margin.bottom;
 
 var parseTime = d3.timeParse("%Y");
 
@@ -15,14 +17,42 @@ const type = {
     FEMAILE: 2
 }
 
-d3.select("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom);
+const chart = d3.select('#chart')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
 
-x = d3.scaleTime().range([0,width]);
-y = d3.scaleLinear().range([height - margin.bottom,0]);
+const innerChart = chart.append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-valueline = d3.line()
-                .x(function(d){ console.log("x", d.date); return d.date;})
-                .y(function(d){ console.log("y", d.value); return d.value;});
+// for testing
+/* innerChart.append("circle")
+.attr("cx", 100)
+.attr("cy", 200)
+.attr("r", 20)
+.attr("fill", "red"); */
+
+var xScale = d3.scaleLinear().range([0,width]);
+var yScale = d3.scaleLinear().range([height, 0]);    
+
+
+var xAxis = d3.axisBottom().scale(xScale);
+
+var yAxis = d3.axisLeft().scale(yScale);
+
+var valueline = d3.line()
+    .x(function(d){ return xScale(d.date);})
+    .y(function(d){ return yScale(d.value);})
+    .curve(d3.curveLinear);
+
+
+// Adds the svg canvas
+var g = innerChart
+    // .call(zoom)
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);    
+
 
 /* var lineFunction = d3.select("svg").line()
                          .x(function(d) { return d.year; })
@@ -94,45 +124,42 @@ function drawChart(data){
         return;
     }
 
-
     //  clean up everything before drawing a new chart
-    d3.selectAll("svg > *").remove();
+    // d3.select("body").selectAll("svg > *").remove();
 
-    x.domain(d3.extent(data[1], function(d) { return parseTime(d.date); }));
-    y.domain([0, 100]);
-
+    xScale.domain(d3.extent(data[1], function(d) { return d.date.toString(); }));
+    yScale.domain([0, 100]);
 
     // Add the X Axis
-    console.log("height",height);
-    d3.select("svg").append("g")
-        .attr("transform", "translate(" + margin.left + "," + (height - margin.bottom) + ")").call(d3.axisBottom(x));
+    console.log("add x axis");
+    innerChart
+        .append('g')
+        .attr('transform', "translate(0," + height + ")")
+        .call(xAxis);
 
+    console.log("add y axis");
     // Add the Y Axis
-    //d3.select("svg").select("g").call(d3.axisLeft(y)).call(g => g.select(".domain").remove());
-    d3.select("svg").append("g").attr("transform", "translate(" + margin.left + "," + (margin.top) + ")").call(d3.axisLeft(y));
+    innerChart
+        .append('g')
+        .call(yAxis)
+        .attr("y", 6);
 
-    d3.select("svg").attr("width", width).attr("height", height)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + (margin.bottom + margin.bottom) + ")")
-        .selectAll()
-        .data(data[1], function(d){return d.value}).enter()
-        .append("circle").attr("r", function(d){return 2}).attr("cx", function(d){return x(parseTime(d.date))}).attr("cy", function(d){return y(d.value)}).attr("fill", "red");
 
-/*         d3.select("svg").attr("width", width + margin.left + margin.right).attr("height",height + margin.top + margin.bottom)
-    .append("g")
-    .attr("width", width + margin.left + margin.right).attr("height",height + margin.top + margin.bottom)
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    console.log("draw data");
+    g.append("path").attr("width", width).attr("height",height)
+    .datum(data[1].map( (d, i) => {
+        console.log("path : date", d.date, "value", d.value);
+        return {
+            date : d.date,
+            value : d.value
+        };
+    }
+    ))
     .attr("class", "line")
-    .attr("fill", "black")
-    .attr("stroke", "steelblue")
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-linecap", "round")
-    .attr("stroke-width", 2)
-    .datum(data[1], function(d){return d.value;})
-    .attr("d", valueline); */
+    .attr("d", valueline);
 
-    //.datum(data);
-    //.attr("d", data[1]);
+    // test if innerChart exists
+    // innerChart.append("rect").attr("width", 100).attr("height", 100).attr("fill", "red");
 };
 
 // callback function
